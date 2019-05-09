caeedc44f87e19621cea6060b1e399de7c44e5e2
Steven <s.vanbennekom@faceworks.nl>
2019-05-09 08:35:20 +0200

    404 page added

--- src/routers/app.js
+++ src/routers/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Homepage from "../pages/homepage";
+import notFound from '../pages/notFound';
 
 const Router = () => {
     return (
@@ -8,6 +9,7 @@ const Router = () => {
             <div>
                 <Switch>
                     <Route exact path="/" component={Homepage}/>
+                    <Route component={notFound}/>
                 </Switch>
             </div>
         </BrowserRouter>
