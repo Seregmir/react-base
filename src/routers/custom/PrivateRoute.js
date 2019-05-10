8f4b8fcb3e90a155b2cc2e733c333fee0407f9a4
Steven <s.vanbennekom@faceworks.nl>
2019-05-10 14:07:06 +0200

    Private route doc update

--- src/routers/custom/PrivateRoute.js
+++ src/routers/custom/PrivateRoute.js
@@ -1,16 +1,18 @@
+/**
+ * @component       PrivateRoute
+ * @description     A wrapper for nodes protected by a statement (boolean from parent),
+ *                  usually used for login protected pages.
+ * @author          Steven van Bennekom <https://stevenvanbennekom.nl>
+ */
+
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
-const PrivateRoute = ({component: Component, isLoggedIn: loggedIn, ...rest}) => {
+const PrivateRoute = ({component: component, statement: statement, ...parentProps}) => {
     return (
         <Route
-            {...rest}
-            render={props => loggedIn ? (
-                <Component {...props}/>
-            ) : (
-                <Redirect to={'/'}/>
-            )}
-        />
+            {...parentProps}
+            render={props => statement ? (<Component {...props}/>) : (<Redirect to={'/'}/>)}/>
     );
 };
 
